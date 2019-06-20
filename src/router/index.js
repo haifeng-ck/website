7bfab5c89c3987f0d4af732455eff1aa39fb7159
zhoujunlong <2530324624@qq.com>
2019-06-20 18:55:27 +0800

    1、一次不成功的修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import header from '@/views/header/header'
+import main from '@/views/main/main'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'header',
+      component: header
+    },
+    {
+      path: '/',
+      name: 'main',
+      component: main
     }
   ]
 })
---
+++ src/views/main/main.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data () {
+    return {
+      msg: '我是main'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
